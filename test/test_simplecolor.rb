137e6fbb12ffc972e5d8ffb4074a371099710019
Damien Robert <damien.olivier.robert+git@gmail.com>
2015-02-27 15:14:36 +0100

    Add more tests

--- test/test_simplecolor.rb
+++ test/test_simplecolor.rb
@@ -3,21 +3,56 @@ require 'simplecolor'
 
 class TestSimpleColor < MiniTest::Test
 
-  def test_version
-    version = SimpleColor.const_get('VERSION')
+	def test_version
+		version = SimpleColor.const_get('VERSION')
 
-    assert(!version.empty?, 'should have a VERSION constant')
-  end
+		assert(!version.empty?, 'should have a VERSION constant')
+	end
 
 end
 
 describe SimpleColor do
-  it "Can be mixed in strings" do
-    SimpleColor.mix_in_string
-    "red".color(:red).must_equal "\e[31mred\e[0m"
-  end
+	before do
+		SimpleColor.enabled=true
+	end
 
-  it "Can be used directly" do
-    SimpleColor.color("red",:red).must_equal "\e[31mred\e[0m"
-  end
+	it "Can be mixed in strings" do
+		SimpleColor.mix_in_string
+		"red".color(:red).must_equal "\e[31mred\e[0m"
+	end
+
+	it "Can be used directly" do
+		SimpleColor.color("red",:red).must_equal "\e[31mred\e[0m"
+	end
+
+	it "can uncolor" do
+		SimpleColor.uncolor(SimpleColor.color("red",:red)).must_equal "red"
+	end
+
+	describe "Shell mode" do
+		before do
+			SimpleColor.enabled=:shell
+		end
+
+		it "Wraps color into shell escapes" do
+			SimpleColor.color("red",:red).must_equal "%{\e[31m%}red%{\e[0m%}"
+		end
+	end
+
+	describe "It can be disabled" do
+		before do
+			SimpleColor.enabled=false
+		end
+
+		it "When disabled color should be a noop" do
+			word="red"
+			SimpleColor.color(word,:red).must_equal word
+		end
+
+		it "When disabled uncolor should be a noop" do
+			word="\e[31mred\e[0m"
+			SimpleColor.uncolor(word).must_equal word
+		end
+	end
+
 end

529452f521b7dc71a700c9970090408f09725ae8
Damien Robert <damien.olivier.robert+git@gmail.com>
2019-04-26 14:15:51 +0200

    bin: --color=number

--- bin/simplecolor.rb
+++ bin/simplecolor.rb
@@ -3,6 +3,13 @@
 require 'simplecolor'
 require 'optparse'
 
+optparse = OptionParser.new do |opt|
+	opt.on("--color=number", "-c", "Set number of colors", Integer) do |v|
+		SimpleColor.color_mode=v
+	end
+end
+optparse.parse!
+
 args=ARGV.map do |arg|
 	if arg.start_with?(':')
 		arg[1..-1].to_sym

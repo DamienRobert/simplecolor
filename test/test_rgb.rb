5a360270b5d1456ef35b450634b5fffe3a11dee6
Damien Robert <damien.olivier.robert+git@gmail.com>
2020-02-23 15:19:48 +0100

    Fix minitest warnings

--- test/test_rgb.rb
+++ test/test_rgb.rb
@@ -3,33 +3,33 @@ require 'simplecolor'
 
 describe SimpleColor::RGB do
 	it "Should output truecolor code" do
-		SimpleColor::RGB.new(10,20,30).ansi.must_equal "38;2;10;20;30"
+		_(SimpleColor::RGB.new(10,20,30).ansi).must_equal "38;2;10;20;30"
 	end
 	it "Can degrade to 256 colors" do
-		SimpleColor::RGB.new(10,20,30).convert(256).ansi.must_equal "38;5;234"
+		_(SimpleColor::RGB.new(10,20,30).convert(256).ansi).must_equal "38;5;234"
 	end
 	it "Can degrade to 16 colors" do
-		SimpleColor::RGB.new(10,20,160).convert(16).ansi.must_equal "34"
+		_(SimpleColor::RGB.new(10,20,160).convert(16).ansi).must_equal "34"
 	end
 	it "Can degrade to 8 colors" do
-		SimpleColor::RGB.new(10,20,30).convert(8).ansi.must_equal "30"
+		_(SimpleColor::RGB.new(10,20,30).convert(8).ansi).must_equal "30"
 	end
 
 	describe "List name" do
 		it "Can specify color names" do
-			SimpleColor::RGB.parse("lavender").to_hex.must_equal "#9F90D0"
+			_(SimpleColor::RGB.parse("lavender").to_hex).must_equal "#9F90D0"
 		end
 		it "Can specify the dict to use for color names" do
-			SimpleColor::RGB.parse("x11:lavender").to_hex.must_equal "#E6E6FA"
+			_(SimpleColor::RGB.parse("x11:lavender").to_hex).must_equal "#E6E6FA"
 		end
 		it "Can use custom names" do
-			SimpleColor::RGB.parse("solarized_base03").to_hex.must_equal "#002B36"
+			_(SimpleColor::RGB.parse("solarized_base03").to_hex).must_equal "#002B36"
 		end
 		it "Custom names have precedence" do
-			SimpleColor::RGB.parse("verydarkbluishgreen").to_hex.must_equal "#002A29"
+			_(SimpleColor::RGB.parse("verydarkbluishgreen").to_hex).must_equal "#002A29"
 			SimpleColor::RGB.color_names["verydarkbluishgreen"]="#002B30"
-			SimpleColor::RGB.parse("verydarkbluishgreen").to_hex.must_equal "#002B30"
-			SimpleColor::RGB.parse("nbs:verydarkbluishgreen").to_hex.must_equal "#002A29"
+			_(SimpleColor::RGB.parse("verydarkbluishgreen").to_hex).must_equal "#002B30"
+			_(SimpleColor::RGB.parse("nbs:verydarkbluishgreen").to_hex).must_equal "#002A29"
 		end
 	end
 end
@@ -40,37 +40,37 @@ describe SimpleColor do
 	end
 
 	it "Can parse a true color name" do
-		SimpleColor.color("foo", "rgb:10-20-30").must_equal "\e[38;2;10;20;30mfoo\e[0m"
-		SimpleColor.color("foo", "10-20-30").must_equal "\e[38;2;10;20;30mfoo\e[0m"
-		SimpleColor.color("foo", "10:20:30").must_equal "\e[38;2;10;20;30mfoo\e[0m"
-		SimpleColor.color("foo", [10, 20, 30]).must_equal "\e[38;2;10;20;30mfoo\e[0m"
+		_(SimpleColor.color("foo", "rgb:10-20-30")).must_equal "\e[38;2;10;20;30mfoo\e[0m"
+		_(SimpleColor.color("foo", "10-20-30")).must_equal "\e[38;2;10;20;30mfoo\e[0m"
+		_(SimpleColor.color("foo", "10:20:30")).must_equal "\e[38;2;10;20;30mfoo\e[0m"
+		_(SimpleColor.color("foo", [10, 20, 30])).must_equal "\e[38;2;10;20;30mfoo\e[0m"
 	end
 
 	it "Can specify 256 colors" do
-		SimpleColor.color("foo", "rgb256:1-2-3").must_equal "\e[38;5;67mfoo\e[0m"
-		SimpleColor.color("foo", "256:1-2-3").must_equal "\e[38;5;67mfoo\e[0m"
-		SimpleColor.color("foo", "256:grey1").must_equal "\e[38;5;233mfoo\e[0m"
-		SimpleColor.color("foo", "256:10").must_equal "\e[38;5;10mfoo\e[0m"
+		_(SimpleColor.color("foo", "rgb256:1-2-3")).must_equal "\e[38;5;67mfoo\e[0m"
+		_(SimpleColor.color("foo", "256:1-2-3")).must_equal "\e[38;5;67mfoo\e[0m"
+		_(SimpleColor.color("foo", "256:grey1")).must_equal "\e[38;5;233mfoo\e[0m"
+		_(SimpleColor.color("foo", "256:10")).must_equal "\e[38;5;10mfoo\e[0m"
 	end
 
 	it "Can specify hexa color name" do
-		SimpleColor.color("foo", "#10AABB").must_equal "\e[38;2;16;170;187mfoo\e[0m"
-		SimpleColor.color("foo", "#1AB").must_equal "\e[38;2;17;170;187mfoo\e[0m"
+		_(SimpleColor.color("foo", "#10AABB")).must_equal "\e[38;2;16;170;187mfoo\e[0m"
+		_(SimpleColor.color("foo", "#1AB")).must_equal "\e[38;2;17;170;187mfoo\e[0m"
 	end
 
 	it "Can specify x11 color name" do
-		SimpleColor.color("foo", "Lemon Chiffon").must_equal "\e[38;2;255;250;205mfoo\e[0m"
-		SimpleColor.color("foo", "lemonchiffon").must_equal "\e[38;2;255;250;205mfoo\e[0m"
+		_(SimpleColor.color("foo", "Lemon Chiffon")).must_equal "\e[38;2;255;250;205mfoo\e[0m"
+		_(SimpleColor.color("foo", "lemonchiffon")).must_equal "\e[38;2;255;250;205mfoo\e[0m"
 	end
 
 	it "Can specify background color name" do
-		SimpleColor.color("foo", "on_lemonchiffon").must_equal "\e[48;2;255;250;205mfoo\e[0m"
-		SimpleColor.color("foo", "on_rgb:255+250+205").must_equal "\e[48;2;255;250;205mfoo\e[0m"
-		SimpleColor.color("foo", "on_#CCBBAA").must_equal "\e[48;2;204;187;170mfoo\e[0m"
-		SimpleColor.color("foo", "on_rgb256:1+2+3").must_equal "\e[48;5;67mfoo\e[0m"
-		SimpleColor.color("foo", "on_rgb256:1").must_equal "\e[48;5;1mfoo\e[0m"
-		SimpleColor.color("foo", "on_rgb256:gray1").must_equal "\e[48;5;233mfoo\e[0m"
-		SimpleColor.color("foo", [:on, 10, 20, 30]).must_equal "\e[48;2;10;20;30mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_lemonchiffon")).must_equal "\e[48;2;255;250;205mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_rgb:255+250+205")).must_equal "\e[48;2;255;250;205mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_#CCBBAA")).must_equal "\e[48;2;204;187;170mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_rgb256:1+2+3")).must_equal "\e[48;5;67mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_rgb256:1")).must_equal "\e[48;5;1mfoo\e[0m"
+		_(SimpleColor.color("foo", "on_rgb256:gray1")).must_equal "\e[48;5;233mfoo\e[0m"
+		_(SimpleColor.color("foo", [:on, 10, 20, 30])).must_equal "\e[48;2;10;20;30mfoo\e[0m"
 	end
 
 	describe "It can fallback to 256 colors" do
@@ -79,14 +79,14 @@ describe SimpleColor do
 		end
 
 		it "Can specify x11 color name" do
-			SimpleColor.color("foo", "Lemon Chiffon").must_equal "\e[38;5;230mfoo\e[0m"
-			SimpleColor.color("foo", "on_Lemon Chiffon").must_equal "\e[48;5;230mfoo\e[0m"
+			_(SimpleColor.color("foo", "Lemon Chiffon")).must_equal "\e[38;5;230mfoo\e[0m"
+			_(SimpleColor.color("foo", "on_Lemon Chiffon")).must_equal "\e[48;5;230mfoo\e[0m"
 		end
 
 		it "Can be forced to be truecolor" do
-			SimpleColor.color("foo", "t:Lemon Chiffon").must_equal "\e[38;2;255;250;205mfoo\e[0m"
-			SimpleColor.color("foo", "truecolor:Lemon Chiffon").must_equal "\e[38;2;255;250;205mfoo\e[0m"
-			SimpleColor.color("foo", "true:on_Lemon Chiffon").must_equal "\e[48;2;255;250;205mfoo\e[0m"
+			_(SimpleColor.color("foo", "t:Lemon Chiffon")).must_equal "\e[38;2;255;250;205mfoo\e[0m"
+			_(SimpleColor.color("foo", "truecolor:Lemon Chiffon")).must_equal "\e[38;2;255;250;205mfoo\e[0m"
+			_(SimpleColor.color("foo", "true:on_Lemon Chiffon")).must_equal "\e[48;2;255;250;205mfoo\e[0m"
 		end
 	end
 
@@ -96,8 +96,8 @@ describe SimpleColor do
 		end
 
 		it "Can specify x11 color name" do
-			SimpleColor.color("foo", "Lemon Chiffon").must_equal "\e[37mfoo\e[0m"
-			SimpleColor.color("foo", "on_Lemon Chiffon").must_equal "\e[47mfoo\e[0m"
+			_(SimpleColor.color("foo", "Lemon Chiffon")).must_equal "\e[37mfoo\e[0m"
+			_(SimpleColor.color("foo", "on_Lemon Chiffon")).must_equal "\e[47mfoo\e[0m"
 		end
 	end
 end
--- test/test_simplecolor.rb
+++ test/test_simplecolor.rb
@@ -14,60 +14,60 @@ describe SimpleColor do
 	end
 
 	it "Can be used directly" do
-		SimpleColor.color("red",:red).must_equal "\e[31mred\e[0m"
+		_(SimpleColor.color("red",:red)).must_equal "\e[31mred\e[0m"
 	end
 
 	it "Can specify several colors" do
-		SimpleColor.color("red",:red,:bold).must_equal "\e[31;1mred\e[0m"
+		_(SimpleColor.color("red",:red,:bold)).must_equal "\e[31;1mred\e[0m"
 	end
 
 	it "Accepts ANSI sequences" do
-		SimpleColor.color("red","\e[31;1m").must_equal "\e[31;1mred\e[0m"
+		_(SimpleColor.color("red","\e[31;1m")).must_equal "\e[31;1mred\e[0m"
 	end
 
 	it "Can mix ANSI sequences and attributes" do
-		SimpleColor.color("red",:inverse,"\e[31m",:blue,:bold).must_equal "\e[7m\e[31m\e[34;1mred\e[0m"
+		_(SimpleColor.color("red",:inverse,"\e[31m",:blue,:bold)).must_equal "\e[7m\e[31m\e[34;1mred\e[0m"
 	end
 
 	it "Can take a block" do
-		SimpleColor.color(:red) { "red" }.must_equal "\e[31mred\e[0m"
+		_(SimpleColor.color(:red) { "red" }).must_equal "\e[31mred\e[0m"
 	end
 
 	it "Can provide only color values" do
-		SimpleColor.color(:red).must_equal "\e[31m"
+		_(SimpleColor.color(:red)).must_equal "\e[31m"
 	end
 
 	it "Does not change the string when there is no color attributes" do
-		SimpleColor.color("foo").must_equal "foo"
+		_(SimpleColor.color("foo")).must_equal "foo"
 	end
 
 	it "can uncolor" do
-		SimpleColor.uncolor(SimpleColor.color("red",:red)).must_equal "red"
+		_(SimpleColor.uncolor(SimpleColor.color("red",:red))).must_equal "red"
 	end
 
 	it "uncolor does not change uncolored strings" do
-		SimpleColor.uncolor("red").must_equal "red"
+		_(SimpleColor.uncolor("red")).must_equal "red"
 	end
 
 	it "Can detect colored strings" do
-		SimpleColor.color?("red").must_equal false
-		SimpleColor.color?(SimpleColor.color("red",:red)).must_equal true
+		_(SimpleColor.color?("red")).must_equal false
+		_(SimpleColor.color?(SimpleColor.color("red",:red))).must_equal true
 	end
 
 	it "Can only show color escape when passed nil" do
-		SimpleColor.color(nil, "x11:lavender").must_equal "\e[38;2;230;230;250m"
+		_(SimpleColor.color(nil, "x11:lavender")).must_equal "\e[38;2;230;230;250m"
 	end
 
 	it "Accepts a block as color parameter" do
-		SimpleColor.color("foo", proc { "x11:lavender" }).must_equal "\e[38;2;230;230;250mfoo\e[0m"
+		_(SimpleColor.color("foo", proc { "x11:lavender" })).must_equal "\e[38;2;230;230;250mfoo\e[0m"
 	end
 
 	it "Proc as color parameter can return multiple values" do
-		SimpleColor.color("foo", proc { ["x11:lavender", :bold] }).must_equal "\e[38;2;230;230;250;1mfoo\e[0m"
+		_(SimpleColor.color("foo", proc { ["x11:lavender", :bold] })).must_equal "\e[38;2;230;230;250;1mfoo\e[0m"
 	end
 
 	it "Has default options" do
-		SimpleColor.opts.must_equal(SimpleColor::Opts.default_opts)
+		_(SimpleColor.opts).must_equal(SimpleColor::Opts.default_opts)
 	end
 
 	describe "Abbreviations" do
@@ -76,26 +76,26 @@ describe SimpleColor do
 		end
 
 		it "Can use the color1 abbreviations" do
-			SimpleColor.color("foo", :color1).must_equal "\e[38;2;230;230;250mfoo\e[0m"
+			_(SimpleColor.color("foo", :color1)).must_equal "\e[38;2;230;230;250mfoo\e[0m"
 		end
 		it "Abbrevations have precedence" do
-			SimpleColor.color("foo", :red).must_equal SimpleColor.color("foo", :green)
+			_(SimpleColor.color("foo", :red)).must_equal SimpleColor.color("foo", :green)
 		end
 	end
 
 	describe "Random" do
 		it "Has a :random color" do
-			SimpleColor.color("foo", :random).must_match(/\e\[38;2;\d+;\d+;\d+mfoo\e\[0m/)
+			_(SimpleColor.color("foo", :random)).must_match(/\e\[38;2;\d+;\d+;\d+mfoo\e\[0m/)
 		end
 		it "Has a :on_random color" do
-			SimpleColor.color("foo", :on_random).must_match(/\e\[48;2;\d+;\d+;\d+mfoo\e\[0m/)
+			_(SimpleColor.color("foo", :on_random)).must_match(/\e\[48;2;\d+;\d+;\d+mfoo\e\[0m/)
 		end
 	end
 
 	describe "Abbreviations" do
 		it "Can specify abbreviations" do
 			SimpleColor.abbreviations[:important]=[:red, :bold]
-			SimpleColor.color("foo", :important).must_equal("\e[31;1mfoo\e[0m")
+			_(SimpleColor.color("foo", :important)).must_equal("\e[31;1mfoo\e[0m")
 		end
 	end
 
@@ -105,24 +105,24 @@ describe SimpleColor do
 		end
 
 		it "Wraps color into shell escapes" do
-			SimpleColor.color("red",:red).must_equal "%{\e[31m%}red%{\e[0m%}"
+			_(SimpleColor.color("red",:red)).must_equal "%{\e[31m%}red%{\e[0m%}"
 		end
 
 		it "Can uncolor correctly" do
-			SimpleColor.uncolor(SimpleColor.color("red",:red)).must_equal "red"
+			_(SimpleColor.uncolor(SimpleColor.color("red",:red))).must_equal "red"
 		end
 
 		it "Only uncolor shell wrapped colors" do
 			red="\e[31mred\e[0m"
-			SimpleColor.uncolor(red).must_equal red
+			_(SimpleColor.uncolor(red)).must_equal red
 		end
 
 		it "Can detect colors correctly" do
-			SimpleColor.color?(SimpleColor.color("red",:red)).must_equal true
+			_(SimpleColor.color?(SimpleColor.color("red",:red))).must_equal true
 		end
 
 		it "Only detect shell wrapped colors" do
-			SimpleColor.color?("\e[31mred\e[0m").must_equal false
+			_(SimpleColor.color?("\e[31mred\e[0m")).must_equal false
 		end
 	end
 
@@ -132,35 +132,35 @@ describe SimpleColor do
 		end
 
 		it "Works on strings" do
-			"red".color(:red).must_equal "\e[31mred\e[0m"
+			_("red".color(:red)).must_equal "\e[31mred\e[0m"
 		end
 
 		it "Uncolor works on strings" do
-			"red".color(:red).uncolor.must_equal "red"
+			_("red".color(:red).uncolor).must_equal "red"
 		end
 
 		it "color does not affects the string" do
 			s="red"
 			s.color(:red)
-			s.must_equal "red"
+			_(s).must_equal "red"
 		end
 
 		it "color! does affects the string" do
 			s="red"
 			s.color!(:red)
-			s.must_equal "\e[31mred\e[0m"
+			_(s).must_equal "\e[31mred\e[0m"
 		end
 
 		it "uncolor does not affects the string" do
 			s="\e[31mred\e[0m"
 			s.uncolor
-			s.must_equal "\e[31mred\e[0m"
+			_(s).must_equal "\e[31mred\e[0m"
 		end
 
 		it "uncolor! does not affects the string" do
 			s="\e[31mred\e[0m"
 			s.uncolor!
-			s.must_equal "red"
+			_(s).must_equal "red"
 		end
 	end
 
@@ -171,41 +171,41 @@ describe SimpleColor do
 
 		it "When disabled color should be a noop" do
 			word="red"
-			SimpleColor.color(word,:red).must_equal word
+			_(SimpleColor.color(word,:red)).must_equal word
 		end
 
 		it "When disabled uncolor should still work" do
 			word="\e[31mred\e[0m"
-			SimpleColor.uncolor(word).must_equal "red"
+			_(SimpleColor.uncolor(word)).must_equal "red"
 		end
 
 		it "Can be disabled punctually" do
 			SimpleColor.enabled=true
 			word="red"
-			SimpleColor.color(word,:red, mode: false).must_equal word
-			SimpleColor.color(word,:red).must_equal "\e[31mred\e[0m"
+			_(SimpleColor.color(word,:red, mode: false)).must_equal word
+			_(SimpleColor.color(word,:red)).must_equal "\e[31mred\e[0m"
 		end
 	end
 
 	describe "It has colors helpers utilities" do
 		it "It can copy one color" do
-			SimpleColor.copy_colors(SimpleColor.color("red",:red),"blue").must_equal "\e[31mblue\e[0m"
+			_(SimpleColor.copy_colors(SimpleColor.color("red",:red),"blue")).must_equal "\e[31mblue\e[0m"
 		end
 
 		it "Copy several colors" do
-			SimpleColor.copy_colors(SimpleColor.color("red",:red,:bold),"blue").must_equal "\e[31;1mblue\e[0m"
+			_(SimpleColor.copy_colors(SimpleColor.color("red",:red,:bold),"blue")).must_equal "\e[31;1mblue\e[0m"
 		end
 
 		it "Can recover colors" do
 			b,e,l=SimpleColor.current_colors(SimpleColor.color("red",:red,:bold))
-			b.must_equal "\e[31;1m"
-			e.must_equal "\e[0m"
-			l.must_equal "red"
+			_(b).must_equal "\e[31;1m"
+			_(e).must_equal "\e[0m"
+			_(l).must_equal "red"
 		end
 
 		it "Can get colors attributes" do
 			b,e=SimpleColor.current_colors(SimpleColor.color("red",:red,:bold))
-			SimpleColor.attributes_from_colors("\e[m"+b+e).must_equal([:reset,:red,:bright,:reset])
+			_(SimpleColor.attributes_from_colors("\e[m"+b+e)).must_equal([:reset,:red,:bright,:reset])
 		end
 
 		it "Can detect 256 and truecolor attributes" do
@@ -213,17 +213,17 @@ describe SimpleColor do
 			expected=[SimpleColor::RGB.new(3, mode: 256),
 				SimpleColor::RGB.new([1,2,3], background: true),
 				:green, :bright, :conceal, :intense_white]
-			SimpleColor.attributes_from_colors(test).must_equal(expected)
+			_(SimpleColor.attributes_from_colors(test)).must_equal(expected)
 		end
 
 		it "Can split a string into color entities" do
-			SimpleColor.color_entities(SimpleColor.color("red",:red,:bold)+SimpleColor.color("blue",:blue)).must_equal(["\e[31;1m", "r", "e", "d", "\e[0m\e[34m", "b", "l", "u", "e", "\e[0m"])
-			SimpleColor.color_entities("blue "+SimpleColor.color("red",:red,:bold)+" green").must_equal(["b", "l", "u", "e", " ", "\e[31;1m", "r", "e", "d", "\e[0m", " ", "g", "r", "e", "e", "n"])
+			_(SimpleColor.color_entities(SimpleColor.color("red",:red,:bold)+SimpleColor.color("blue",:blue))).must_equal(["\e[31;1m", "r", "e", "d", "\e[0m\e[34m", "b", "l", "u", "e", "\e[0m"])
+			_(SimpleColor.color_entities("blue "+SimpleColor.color("red",:red,:bold)+" green")).must_equal(["b", "l", "u", "e", " ", "\e[31;1m", "r", "e", "d", "\e[0m", " ", "g", "r", "e", "e", "n"])
 		end
 
 		it "Can split a string into color strings" do
-			SimpleColor.color_strings(SimpleColor.color("red",:red,:bold)+SimpleColor.color("blue",:blue)).must_equal(["\e[31;1m", "red", "\e[0m\e[34m", "blue", "\e[0m"])
-			SimpleColor.color_strings("blue "+SimpleColor.color("red",:red,:bold)+" green").must_equal(["blue ", "\e[31;1m", "red", "\e[0m", " green"])
+			_(SimpleColor.color_strings(SimpleColor.color("red",:red,:bold)+SimpleColor.color("blue",:blue))).must_equal(["\e[31;1m", "red", "\e[0m\e[34m", "blue", "\e[0m"])
+			_(SimpleColor.color_strings("blue "+SimpleColor.color("red",:red,:bold)+" green")).must_equal(["blue ", "\e[31;1m", "red", "\e[0m", " green"])
 		end
 	end
 
@@ -233,30 +233,30 @@ describe SimpleColor do
 		end
 
 		it "Can color too" do
-			SimpleColor2.color("red", :red).must_equal "\e[31mred\e[0m"
+			_(SimpleColor2.color("red", :red)).must_equal "\e[31mred\e[0m"
 		end
 
 		it "Still works if SimpleColor is disabled" do
 			SimpleColor.enabled=false
-			SimpleColor2.color("red", :red).must_equal "\e[31mred\e[0m"
-			SimpleColor.color("red", :red).must_equal "red"
+			_(SimpleColor2.color("red", :red)).must_equal "\e[31mred\e[0m"
+			_(SimpleColor.color("red", :red)).must_equal "red"
 		end
 
 		it "Can be disabled without disabling SimpleColor" do
 			SimpleColor2.enabled=false
-			SimpleColor.color("red", :red).must_equal "\e[31mred\e[0m"
-			SimpleColor2.color("red", :red).must_equal "red"
+			_(SimpleColor.color("red", :red)).must_equal "\e[31mred\e[0m"
+			_(SimpleColor2.color("red", :red)).must_equal "red"
 			SimpleColor2.enabled=true
 		end
 
 		it "Can be mixed in a class" do
 			string_class=Class.new(String)
 			SimpleColor2.mix_in(string_class)
-			string_class.new("red").color(:red).must_equal "\e[31mred\e[0m"
+			_(string_class.new("red").color(:red)).must_equal "\e[31mred\e[0m"
 			SimpleColor.enabled=false
-			string_class.new("red").color(:red).must_equal "\e[31mred\e[0m"
+			_(string_class.new("red").color(:red)).must_equal "\e[31mred\e[0m"
 			SimpleColor2.enabled=false
-			string_class.new("red").color(:red).must_equal "red"
+			_(string_class.new("red").color(:red)).must_equal "red"
 			SimpleColor2.enabled=true
 		end
 
@@ -268,72 +268,72 @@ describe SimpleColor do
 				extend SimpleColor
 			end
 			SimpleColor.opts=default_opts
-			SimpleColor3.opts.must_equal default_opts
+			_(SimpleColor3.opts).must_equal default_opts
 		end
 	end
 
 	describe "It raises invalid parameters" do
 		it "Raises when we pass invalid parameter" do
-			proc { SimpleColor.color("foo", mode: :garbage)}.must_raise SimpleColor::WrongParameter
+			_(proc { SimpleColor.color("foo", mode: :garbage)}).must_raise SimpleColor::WrongParameter
 		end
 
 		it "Raises when we pass invalid rgb parameter" do
-			proc { SimpleColor.color("foo", "x11:lavender", color_mode:10)}.must_raise SimpleColor::WrongRGBParameter
+			_(proc { SimpleColor.color("foo", "x11:lavender", color_mode:10)}).must_raise SimpleColor::WrongRGBParameter
 		end
 
 		it "Raises when we pass an invalid color" do
-			proc { SimpleColor.color("foo", SimpleColor)}.must_raise SimpleColor::WrongColor
-			proc { SimpleColor.color("foo", "nonexistingcolorname")}.must_raise SimpleColor::WrongRGBColor
+			_(proc { SimpleColor.color("foo", SimpleColor)}).must_raise SimpleColor::WrongColor
+			_(proc { SimpleColor.color("foo", "nonexistingcolorname")}).must_raise SimpleColor::WrongRGBColor
 		end
 	end
 
 	describe "Chain colors" do
 		it "Can chain global colors" do
 			r=SimpleColor["red", :red]
-			SimpleColor.color(r, :blue).must_equal "\e[31m\e[34mred\e[0m"
+			_(SimpleColor.color(r, :blue)).must_equal "\e[31m\e[34mred\e[0m"
 		end
 
 		it "Can keep global colors" do
 			r=SimpleColor["red", :red]
-			SimpleColor.color(r, :blue, global_color: :keep).must_equal SimpleColor["red", :red]
+			_(SimpleColor.color(r, :blue, global_color: :keep)).must_equal SimpleColor["red", :red]
 		end
 
 		it "Can give precedence to existing global colors" do
 			r=SimpleColor["red", :red]
-			SimpleColor.color(r, :blue, global_color: :before).must_equal "\e[34m\e[31mred\e[0m"
+			_(SimpleColor.color(r, :blue, global_color: :before)).must_equal "\e[34m\e[31mred\e[0m"
 		end
 
 		it "Will give precedence to local colors" do
 			r="foo "+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue).must_equal "\e[34mfoo \e[31mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue)).must_equal "\e[34mfoo \e[31mred\e[0m\e[34m bar\e[0m"
 			r="foo "+SimpleColor["red", :red]
-			SimpleColor.color(r, :blue).must_equal "\e[34mfoo \e[31mred\e[0m"
+			_(SimpleColor.color(r, :blue)).must_equal "\e[34mfoo \e[31mred\e[0m"
 			r=+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue).must_equal "\e[34m\e[31mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue)).must_equal "\e[34m\e[31mred\e[0m\e[34m bar\e[0m"
 		end
 
 		it "Can get precedence over local colors" do
 			r="foo "+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue, local_color: :after).must_equal "\e[34mfoo \e[31m\e[34mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :after)).must_equal "\e[34mfoo \e[31m\e[34mred\e[0m\e[34m bar\e[0m"
 			r="foo "+SimpleColor["red", :red]
-			SimpleColor.color(r, :blue, local_color: :after).must_equal "\e[34mfoo \e[31m\e[34mred\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :after)).must_equal "\e[34mfoo \e[31m\e[34mred\e[0m"
 			r=+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue, local_color: :after).must_equal "\e[31m\e[34mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :after)).must_equal "\e[31m\e[34mred\e[0m\e[34m bar\e[0m"
 		end
 
 		it "Can keep local colors" do
 			r="foo "+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue, local_color: :keep).must_equal "\e[34mfoo \e[0m\e[31mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :keep)).must_equal "\e[34mfoo \e[0m\e[31mred\e[0m\e[34m bar\e[0m"
 			r="foo "+SimpleColor["red", :red]
-			SimpleColor.color(r, :blue, local_color: :keep).must_equal "\e[34mfoo \e[0m\e[31mred\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :keep)).must_equal "\e[34mfoo \e[0m\e[31mred\e[0m"
 			r=+SimpleColor["red", :red]+" bar"
-			SimpleColor.color(r, :blue, local_color: :keep).must_equal "\e[31mred\e[0m\e[34m bar\e[0m"
+			_(SimpleColor.color(r, :blue, local_color: :keep)).must_equal "\e[31mred\e[0m\e[34m bar\e[0m"
 		end
 
 		it "Can store precedence settings" do
 			SimpleColor.opts[:global_color]=:keep
 			r=SimpleColor["red", :red]
-			SimpleColor.color(r, :blue).must_equal(SimpleColor["red", :red])
+			_(SimpleColor.color(r, :blue)).must_equal(SimpleColor["red", :red])
 		end
 	end
 end
